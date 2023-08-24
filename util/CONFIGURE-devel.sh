a1b801d2ddd34a94abb95193e14954c08768f634
Darryl Sokoloski <darryl@sokoloski.ca>
2023-08-24 03:02:47 +0000

    Added support for thread sanitizer.

--- util/CONFIGURE-devel.sh
+++ util/CONFIGURE-devel.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 : ${COMPILER:=gcc}
+: ${CPPFLAGS:="-pipe -g -O1 -fexceptions -Wall"}
+: ${LDFLAGS:=}
 : ${VARIANT:=generic}
+: ${ENABLE_SANITIZER:=false}
+: ${ENABLE_STACK_PROTECTION:=false}
 : ${OPTION_CONNTRACK:=enable}
 : ${OPTION_NETLINK:=enable}
 : ${OPTION_PLUGINS:=enable}
 : ${OPTION_LIBTCMALLOC:=enable}
 : ${OPTION_NFQUEUE:=enable}
-: ${ENABLE_SANITIZER:=false}
-: ${ENABLE_STACK_PROTECTION:=false}
 
 : ${prefix:=/usr}
 : ${exec_prefix:=${prefix}}
@@ -26,24 +28,9 @@
 
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 
-CPPFLAGS_COMMON="-pipe -g -O1 -fexceptions -Wall"
-
-case "x${ENABLE_SANITIZER}" in
-xaddress)
-  ;;
-xthread)
-  ;;
-xfalse)
-  ;;
-*)
-  echo "ERROR: Unsupported SANITIZER: ${ENABLE_SANITIZER}"
-  exit 1
-  ;;
-esac
-
-if [ "x${ENABLE_SANITIZER}" != "xfalse" ]; then
+if [ "x${ENABLE_SANITIZER}" == "xtrue" ]; then
   if [ "x${COMPILER}" == "xgcc" ]; then
-    echo "Overriding COMPILER to clang, sanitzer enabled."
+    echo "Overriding COMPILER to clang, sanitizer enabled."
     COMPILER=clang
   fi
   if [ "x${OPTION_LIBTCMALLOC}" == "xenable" ]; then
@@ -55,13 +42,14 @@ fi
 if [ "x${COMPILER}" == "xgcc" ]; then
   export CC=gcc
   export CXX=g++
-  CPPFLAGS_COMMON="${CPPFLAGS_COMMON} -grecord-gcc-switches"
+  CPPFLAGS="${CPPFLAGS} -grecord-gcc-switches"
 elif [ "x${COMPILER}" == "xclang" ]; then
   export CC=clang
   export CXX=clang++
+
   if [ "x${ENABLE_SANITIZER}" != "xfalse" ]; then
-    export LDFLAGS="-fsanitize=${ENABLE_SANITIZER}"
-    CPPFLAGS_COMMON="${CPPFLAGS_COMMON} -fsanitize=${ENABLE_SANITIZER} -fno-omit-frame-pointer"
+    CPPFLAGS+="-fsanitize=${ENABLE_SANITIZER} -fno-omit-frame-pointer"
+    LDFLAGS+="-fsanitize=${ENABLE_SANITIZER}"
   fi
 else
   echo "ERROR: Unsupported COMPILER: ${COMPILER}"
@@ -69,26 +57,12 @@ else
 fi
 
 if [ "x${ENABLE_STACK_PROTECTION}" == "xtrue" ]; then
-  CPPFLAGS_COMMON="${CPPFLAGS_COMMON} -fstack-clash-protection \
+  CPPFLAGS="${CPPFLAGS} -fstack-clash-protection \
     -fstack-protector-strong -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2"
 fi
 
-export CPPFLAGS="${CPPFLAGS_COMMON}"
-
-echo "Options:"
-echo " COMPILER: ${COMPILER}"
-echo " VARIANT: ${VARIANT}"
-echo " OPTION_CONNTRACK: ${OPTION_CONNTRACK}"
-echo " OPTION_NETLINK: ${OPTION_NETLINK}"
-echo " OPTION_PLUGINS: ${OPTION_PLUGINS}"
-echo " OPTION_LIBTCMALLOC: ${OPTION_LIBTCMALLOC}"
-echo " OPTION_NFQUEUE: ${OPTION_NFQUEUE}"
-echo " ENABLE_SANITIZER: ${ENABLE_SANITIZER}"
-echo " ENABLE_STACK_PROTECTION: ${ENABLE_STACK_PROTECTION}"
-echo " CPPFLAGS: ${CPPFLAGS}"
-echo " LDFLAGS: ${LDFLAGS}"
-
-if [ $# -gt 0 -a "x$1" == "xhelp" ]; then exit 0; fi
+export CPPFLAGS="${CPPFLAGS}"
+export LDFLAGS+="${LDFLAGS}"
 
 case "x${VARIANT}" in
 xgeneric)
@@ -102,6 +76,21 @@ xubuntu20x)
   exit 1
   ;;
 esac
+
+echo "Options:"
+echo " COMPILER: ${COMPILER}"
+echo " CPPFLAGS: ${CPPFLAGS}"
+echo " LDFLAGS: ${LDFLAGS}"
+echo " VARIANT: ${VARIANT}"
+echo " ENABLE_SANITIZER: ${ENABLE_SANITIZER}"
+echo " ENABLE_STACK_PROTECTION: ${ENABLE_STACK_PROTECTION}"
+echo " OPTION_CONNTRACK: ${OPTION_CONNTRACK}"
+echo " OPTION_NETLINK: ${OPTION_NETLINK}"
+echo " OPTION_PLUGINS: ${OPTION_PLUGINS}"
+echo " OPTION_LIBTCMALLOC: ${OPTION_LIBTCMALLOC}"
+echo " OPTION_NFQUEUE: ${OPTION_NFQUEUE}"
+
+if [ $# -gt 0 -a "x$1" == "xhelp" ]; then exit 0; fi
 
 ./configure \
     --program-prefix= \
